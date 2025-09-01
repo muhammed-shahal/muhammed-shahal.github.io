ac19a98fc49881c885641bf07a9df42d1c04aca8
Musthafa Vakkayil <musthuvakkayil@gmail.com>
2025-09-01 21:16:42 +0530

    fix: update resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -7,7 +7,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/muhammed-shahal",
   resume:
-    "https://drive.google.com/file/d/1WqNwEXCJbfMNi8xBxdpke0dY_FFyDAzZ/view?usp=sharing",
+    "https://drive.google.com/file/d/1GVTPUKXctVtgk33vyUdMUJJx4bcuf9ur/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/musthafashahal/",
 };
 
